5c0c36898b5736530754baa26f82f682532436e0
mick shannahan <beasticorn@gmail.com>
2022-11-16 23:31:54 -0700

    updated starting package

--- cat-query-shipping/server/models/Account.js
+++ cat-query-shipping/server/models/Account.js
@@ -18,7 +18,7 @@ export const AccountSchema = new Schema(
     credits: { type: Number, default: 0 },
     totalCredits: { type: Number, default: 0 },
     components: { type: Number, default: 0 },
-    lostShipmentId: { type: Schema.Types.ObjectId, ref: 'Shipment', default: '62361f3836ac2a5a2a7eab53' },
+    lostShipmentId: { type: Schema.Types.ObjectId, ref: 'Shipment', default: '6375d2e453dbc622793735d0' },
     currentGuesses: [{ type: Schema.Types.ObjectId }],
     shipmentsFound: [{ type: Schema.Types.ObjectId }],
     shipmentsAbandoned: [{ type: Schema.Types.ObjectId }],
