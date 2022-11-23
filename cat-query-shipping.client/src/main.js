7e7264ae117f74f822238c10ed4a749d8c299f02
mick shannahan <beasticorn@gmail.com>
2022-11-23 08:10:04 -0700

    fixed popper getting stuck

--- cat-query-shipping.client/src/main.js
+++ cat-query-shipping.client/src/main.js
@@ -17,17 +17,17 @@ const root = createApp(App)
 registerGlobalComponents(root)
 
 root
-  .directive('pickup',{
-    beforeUnmount:(el, binding) => {
+  .directive('pickup', {
+    beforeUnmount: (el, binding) => {
       el.removeEventListener('mousedown', binding.dir.pickupElm)
       el.removeEventListener('mouseup', binding.dir.dropElm)
       el.remove()
     },
-    updated:(el, binding)=>{
+    updated: (el, binding) => {
       let data = JSON.parse(el.dataset.pickup)?.id
     },
-    mounted:(el, binding) =>{
-      el.ondragstart= ()=> false
+    mounted: (el, binding) => {
+      el.ondragstart = () => false
       el.style.cursor = 'grab'
       el.style.position = 'absolute'
       el.style.transition = 'transform 0.2s ease'
@@ -48,14 +48,14 @@ root
       let dropW = 0
       let dropH = 0
       let lag = 0
-      function drag({movementX, movementY, clientX, clientY}){
+      function drag({ movementX, movementY, clientX, clientY }) {
         el.hidden = true;
         let elmBelow = document.elementFromPoint(event.clientX, event.clientY);
         el.hidden = false;
         droppable = elmBelow.closest('.drop-zone');
 
         //SECTION in drop zone
-        if(droppable) {
+        if (droppable) {
           inDrop = true
           el.style.filter = 'drop-shadow(0px 0px 2px white)'
           let dropRect = droppable.getBoundingClientRect()
@@ -65,16 +65,16 @@ root
           dropZ = dropStyle.zIndex
           dropH = dropRect.height
           dropW = dropRect.width
-        } else {  el.style.filter = null; inDrop = false; droppable = null }
+        } else { el.style.filter = null; inDrop = false; droppable = null }
 
         let style = window.getComputedStyle(el)
         let left = parseInt(style.left)
         let top = parseInt(style.top)
-        if(left + width + movementX < screen.clientWidth - 30 && left + movementX > 15){
-          el.style.left =  clientX - (width/2) + 'px'
+        if (left + width + movementX < screen.clientWidth - 30 && left + movementX > 15) {
+          el.style.left = clientX - (width / 2) + 'px'
         }
-        if(top  + movementY >  15 && top + height + movementY < screen.clientHeight -15){
-          el.style.top = clientY - (height/2) + 'px'
+        if (top + movementY > 15 && top + height + movementY < screen.clientHeight - 15) {
+          el.style.top = clientY - (height / 2) + 'px'
         }
 
         // NOTE ROTATE ONLY WORKS WHEN CONSOLE OPEN
@@ -83,68 +83,73 @@ root
         let rot = movementX > 0 ? 5 : movementX < 0 ? -5 : 0
         el.style.transform = `rotate(${rot}deg) scale(1.02)`
       }
-        //SECTION Pickup
-        binding.dir.pickupElm = ()=> {
-          binding.value(event)
-          sessionStorage.setItem('pickup'  , el.dataset.pickup)
-          el.addEventListener('mousemove', drag)
-          el.style.transform = 'scale(1.02)'
-          el.style.zIndex= 10000
-        }
-        el.addEventListener('mousedown', binding.dir.pickupElm)
+      //SECTION Pickup
+      binding.dir.pickupElm = () => {
+        binding.value(event)
+        sessionStorage.setItem('pickup', el.dataset.pickup)
+        el.addEventListener('mousemove', drag)
+        el.style.transform = 'scale(1.02)'
+        el.style.zIndex = 10000
+      }
+      el.addEventListener('mousedown', binding.dir.pickupElm)
 
-        //SECTION Drop
-        binding.dir.dropElm = ()=> {
-          el.removeEventListener('mousemove', drag)
-          el.hidden = true;
-          el.hidden = false;
-          el.style.transform = `scale(1) ${startingRot}`
-          el.style.zIndex=startingZ
-          // FIXME this snapping needs correcting
-          if(binding.arg == 'anchor' &&  !inDrop){
-            el.style.left = startingLeft
-            el.style.top = startingTop
-          }
-          if(inDrop){
-            if(droppable.drop) droppable.drop()
-            if(droppable.dataset.anchor){
-              el.style.left = dropX + (dropW/2) - (elmRect.width/2) + 'px'
-              el.style.top = dropY + (dropH/2) - (elmRect.height/2) + 'px'
-            }
-            el.style.zIndex = dropZ + 1
-            inDrop=false
-          }
+      //SECTION Drop
+      binding.dir.dropElm = () => {
+        el.removeEventListener('mousemove', drag)
+        el.hidden = true;
+        el.hidden = false;
+        el.style.transform = `scale(1) ${startingRot}`
+        el.style.zIndex = startingZ
+        // FIXME this snapping needs correcting
+        if (binding.arg == 'anchor' && !inDrop) {
+          el.style.left = startingLeft
+          el.style.top = startingTop
         }
-        el.addEventListener('mouseup', binding.dir.dropElm)
+        if (inDrop) {
+          if (droppable.drop) droppable.drop()
+          if (droppable.dataset.anchor) {
+            el.style.left = dropX + (dropW / 2) - (elmRect.width / 2) + 'px'
+            el.style.top = dropY + (dropH / 2) - (elmRect.height / 2) + 'px'
+          }
+          el.style.zIndex = dropZ + 1
+          inDrop = false
+        }
+      }
+      el.addEventListener('mouseup', binding.dir.dropElm)
     }
   })
   .directive('drop', {
-    mounted:(el, binding)=>{
+    mounted: (el, binding) => {
       el.classList.add('drop-zone')
-      if(binding.arg == 'anchor') {
+      if (binding.arg == 'anchor') {
         el.setAttribute('data-anchor', true)
       }
       el.style.zIndex = 500
-        el.drop = ()=> {
-          if(binding.value) binding.value(event, JSON.parse(sessionStorage.getItem('pickup')))
-        }
+      el.drop = () => {
+        if (binding.value) binding.value(event, JSON.parse(sessionStorage.getItem('pickup')))
+      }
     }
   })
   .directive('tooltip', {
     mounted: (el, binding) => {
       el.title = binding.value
       let tip = new Tooltip(el, {
-        placement: binding.arg,
-        offset: [10,0],
+        trigger: 'hover',
+        placement: binding.arg, // left, right, top, bottom
+        // offset: [10, 0],
         popperConfig: {}
       })
+      el.addEventListener('onmouseleave', tip.hide())
     },
-    updated: (el, binding) => {
-      el.title = binding.value
-      let tip = new Tooltip(el, {
-        placement: binding.arg
-      })
-    }
+    // FIXME gets stuck on clicked elements
+    // updated: (el, binding) => {
+    //   el.title = binding.value
+    //   let tip = new Tooltip(el, {
+    //     trigger: 'hover',
+    //     placement: binding.arg
+    //   })
+    //   el.addEventListener('onmouseleave', tip.hide())
+    // }
   })
   .use(Vue3Tour)
   .use(router)
