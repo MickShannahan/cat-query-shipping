d93ac48642b4938f479431842f0c4518a8307653
mick shannahan <beasticorn@gmail.com>
2023-04-05 23:11:09 -0600

    un pushed changes

---
+++ cat-query-shipping/client/assets/AboutPage.d52651fd.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.e00feda2.js";import{o,d as t,e as s}from"./vendor.7241f53f.js";const a={name:"AboutPage"},n={class:"about"},r=s("h1",null,"This is the about page",-1),c=[r];function _(d,i,f,p,u,h){return o(),t("div",n,c)}var b=e(a,[["render",_]]);export{b as default};
