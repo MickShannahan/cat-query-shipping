75db49095890757959408c05cfc9606ea7c083f1
mick shannahan <beasticorn@gmail.com>
2023-04-22 12:20:59 -0600

    build small fixes

---
+++ cat-query-shipping/client/assets/AboutPage.fc1f0f8e.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.e905566a.js";import{o,d as t,e as s}from"./vendor.80559bff.js";const a={name:"AboutPage"},n={class:"about"},r=s("h1",null,"This is the about page",-1),c=[r];function _(d,i,p,f,u,h){return o(),t("div",n,c)}var b=e(a,[["render",_]]);export{b as default};
