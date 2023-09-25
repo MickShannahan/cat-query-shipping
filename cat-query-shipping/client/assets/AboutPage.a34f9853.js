6ecf14c3e1a6bfe8e15495cb08035665daf39e5f
mick shannahan <beasticorn@gmail.com>
2023-09-25 08:07:05 -0600

    broken page

---
+++ cat-query-shipping/client/assets/AboutPage.a34f9853.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.feb716f7.js";import{o,d as t,e as s}from"./vendor.80559bff.js";const a={name:"AboutPage"},n={class:"about"},r=s("h1",null,"This is the about page",-1),c=[r];function _(d,i,f,p,u,h){return o(),t("div",n,c)}var b=e(a,[["render",_]]);export{b as default};
